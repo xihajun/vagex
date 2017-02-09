3cd5c4c14c3e964f75c7d0fda84b684df10c49c1
xihajun <www.junfan.com@gmail.com>
2017-02-09 13:18:19 +0800

    Update vagex.sh

--- vagex.sh
+++ vagex.sh
@@ -58,19 +58,20 @@ echo "================================================="
 #安装相应的软件
 
 if [ "$release" == "CentOS" ]; then
-  echo "A.CentOS 5.x"
-  echo "B.CentOS 6.x"
-  echo "请选择相应的发行版本系统（输入数字序号）："
+  
+  read -p "请选择相应的发行版本系统（输入数字序号）：" number  
+  echo "1.CentOS 5.x"
+  echo "2.CentOS 6.x"
   #read number
-  if [ "$?" == "" ]; then
+  if [ "$number" == "" ]; then
       echo "未选择任何发行版本，脚本退出"
   exit 1
-  elif ["$?" == "A"];then
+  elif ["$number" == "1"];then
     echo "您选择的操作系统是CentOS 5.x"
     echo "正在为您安装软件"
     yum groupinstall xfce-4.4
     yum install vnc vnc-server
-  elif ["$?" == "B"];then
+  elif ["$number" == "2"];then
     echo "您选择的操作系统是CentOS 6.x"
     echo "正在为您安装软件"
     wget https://raw.githubusercontent.com/catonisland/Vagex-For-CentOS-6/master/epel-release-6-8.noarch.rpm;
