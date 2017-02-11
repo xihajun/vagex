7cdc82a621ccd8d3b1b7f34dfa8d8c3bfd00da68
xihajun <www.junfan.com@gmail.com>
2017-02-11 22:50:32 +0800

    Update vagex.sh

--- vagex.sh
+++ vagex.sh
@@ -87,6 +87,76 @@ if [ "$release" == "CentOS" ]; then
     exit 1
   fi
 fi
+if [ "$release" == "Debian" ]; then
+  echo "1.Debian 7.x"
+  echo "2.Debian 8.x"
+  read -p "请选择相应的发行版本系统（输入数字序号）：" number
+  if [ "$number" == "" ]; then
+    echo "未选择任何发行版本，脚本退出"
+    exit 1
+    elif [ "$number" == "1" ];then
+    echo "您选择的操作系统是Debian 7.x"
+    echo "正在为您安装软件"
+    #更新
+    apt-get update
+    apt-get upgrade -y
+    #安装必要软件
+    apt-get install -y gnome
+    apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
+    apt-get install -y tightvncserver
+    #配置文件
+    cat > /etc/sysconfig/vncservers<<EOF
+    #!/bin/shxrdb $HOME/.Xresources
+    xsetroot -solid grey
+    x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &
+    # x-window-manager &
+    gnome-session &
+    EOF
+    #开启服务
+    echo "启动vnc服务，请输入密码（ 不少于6位）"
+    tightvncserver -geometry 1024x720 :1
+    echo "================================================="
+    echo "安装完成！"
+    echo "感谢支持嘻哈小屋 :P"
+    echo "请重启服务器以让配置生效"
+    echo "================================================="
+    exit 1
+    #关闭防火墙（重启后生效）
+    #chkconfig iptables off
+  elif [ "$number" == "2" ];then
+    echo "您选择的操作系统是Debian 8.x"
+    echo "正在为您安装软件"
+    #更新
+    apt-get update
+    apt-get upgrade -y
+    #安装必要软件
+    apt-get install -y gnome
+    apt-get install xfce4 xfce4-goodies gnome-icon-theme tightvncserver
+    apt-get install iceweasel
+    #新增用户
+    adduser vnc
+    echo "新建vnc用户，请输入密码"
+    apt-get install sudo
+    gpasswd -a vnc sudo
+    su - vnc
+    echo "启动vnc服务，请输入密码（ 不少于6位）"
+    vncserver
+    echo "================================================="
+    echo "安装完成！"
+    echo "感谢支持嘻哈小屋 :P"
+    echo "请重启服务器以让配置生效"
+    echo "================================================="
+    #关闭防火墙（重启后生效）
+    #chkconfig iptables off
+    exit 1
+  else
+    echo "目前该脚本只支持CentOS 5.x、CentOS 6.x，其他系统会在以后更新"
+    echo "感谢支持嘻哈小屋！"
+    echo "欢迎您的再次来访：www.edu-ing.cn"
+    exit 1
+  fi
+fi
+
 if [[ "$release" == "Ubuntu" ]] || [[ "$release" == "Debian" ]]; then
   echo "暂时不支持该操作系统"
   echo "目前该脚本只支持CentOS 5.x、CentOS 6.x，其他系统会在以后更新"
