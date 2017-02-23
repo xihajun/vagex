f983674709640ebe4d7a5ed3f0a5904a468f6c48
xihajun <www.junfan.com@gmail.com>
2017-02-23 16:43:08 +0800

    Update vagex_d7.sh

--- Debian7.x/vagex_d7.sh
+++ Debian7.x/vagex_d7.sh
@@ -16,12 +16,51 @@ x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &
 gnome-session &
 EOF
 #开启服务
-echo "启动vnc服务，请输入密码（ 不少于6位）"
+echo "启动vnc服务，请输入密码（ 不少于8位）"
 tightvncserver -geometry 1024x720 :1
+echo "启动成功，暂停配置中。。。"
+#暂停vnc
+tightvncserver -kill :1
+#设置开机启动
+cat > /etc/init.d/tightvncserver<<EOF
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides: tightvncserver
+# Required-Start: $syslog $remote_fs $network
+# Required-Stop: $syslog $remote_fs $network
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Starts VNC Server on system start.
+# Description: Starts tight VNC Server. Script written by James Swineson.
+### END INIT INFO
+# /etc/init.d/tightvncserver
+VNCUSER='root'
+case "$1" in
+start)
+su $VNCUSER -c '/usr/bin/tightvncserver -geometry 800x600 -depth 24 :1'
+echo "Starting TightVNC Server for $VNCUSER"
+;;
+stop)
+pkill Xtightvnc
+echo "TightVNC Server stopped"
+;;
+*)
+echo "Usage: /etc/init.d/tightvncserver {start|stop}"
+exit 1
+;;
+esac
+exit 0`</pre>
+EOF
+#修改权限
+chmod 755 /etc/init.d/tightvncserver
+update-rc.d tightvncserver defaults
+#重启vnc
+tightvncserver :1
+
 echo "================================================="
 echo "安装完成！"
 echo "感谢支持嘻哈小屋 :P"
 echo "请重启服务器以让配置生效"
-echo "暂未设置开机自启"
+echo "已经设为开机自启"
 echo "================================================="
 exit 1
